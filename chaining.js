ca0f84c627d62a0072f0e445381ad8e68b706dd2
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-06-15 16:00:11 -0500

    added in 2 more chains

--- chaining.js
+++ chaining.js
@@ -7,11 +7,22 @@ const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
 integers.sort(function(a, b){return b - a})
 
-console.log(integers)
+console.log("decending", integers)
 
 // Remove any integers greater than 19.
 
 const newArray = integers.sort(function(a, b){return b - a}).filter(function (number){return number <= 19})
 
 
-console.log(newArray)
+console.log("less than 19", newArray)
+
+// Multiply each remaining number by 1.5 and then subtract 1.
+
+const newerArray = integers.sort(function(a, b){return b - a}).filter(function (number){return number <= 19}).map(function (number1){return number1 * 1.5 - 1})
+
+console.log("multiply by 1.5", newerArray)
+
+// Then output (either in the DOM or the console) the sum of all the resulting numbers.
+const newestArray = integers.sort(function(a, b){return b - a}).filter(function (number){return number <= 19}).map(function (number1){return number1 * 1.5 - 1}).reduce(function (number2, next){return number2 + next})
+
+console.log("add them up", newestArray)
